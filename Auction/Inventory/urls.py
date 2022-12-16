ae505ed0d964ea5462ea7aea22b617c534b2a634
sarath <sarath.webkrone@gmail.com>
2022-12-16 18:12:40 +0530

    Creating a new templates and rendering the page

--- Auction/Inventory/urls.py
+++ Auction/Inventory/urls.py
@@ -3,6 +3,7 @@ from . import views
 app_name = 'inventory'
 
 urlpatterns = [
-    path('', views.inventory, name='inventory_index')
+    path('', views.inventory, name='inventory_index'),
+    path('home/', views.home, name='inventory_home')
 	
 ]
--- Auction/Inventory/views.py
+++ Auction/Inventory/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def inventory(request):
 	return render(request,'index.html')
+
+def home(request):
+    return render(request, 'home.html')	
