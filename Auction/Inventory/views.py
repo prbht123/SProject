e12c7addbc804bfb2518c6359b394c322efca5c6
sarath <sarath.webkrone@gmail.com>
2022-12-20 12:10:25 +0530

    Done in views and templates rendering the page

--- Auction/Inventory/views.py
+++ Auction/Inventory/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def inventory(request):
 	return render(request,'index.html')
+
+def text(request):
+	return render(request,'text.html')
