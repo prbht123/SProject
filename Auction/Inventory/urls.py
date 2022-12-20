d665c808fe0e77b71c6f21d7c8fd2fd1dc2492da
sarath <sarath.webkrone@gmail.com>
2022-12-20 11:19:28 +0530

    Have done some changes in templates

--- Auction/Inventory/urls.py
+++ Auction/Inventory/urls.py
@@ -3,6 +3,8 @@ from . import views
 app_name = 'inventory'
 
 urlpatterns = [
-    path('', views.inventory, name='inventory_index')
+    path('', views.inventory, name='inventory_index'),
+    path('training/', views.training, name='inventory_training')
+
 	
 ]
